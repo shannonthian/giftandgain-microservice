d585a83f692b28682527a810b3958fac7c40ff87
Sim Yin Ci <simyinci@gmail.com>
2023-09-03 19:25:48 +0800

    Added Current Inventory Entity

--- frontend/src/main/webapp/app/entities/menu.tsx
+++ frontend/src/main/webapp/app/entities/menu.tsx
@@ -7,6 +7,9 @@ const EntitiesMenu = () => {
   return (
     <>
       {/* prettier-ignore */}
+      <MenuItem icon="warehouse" to="/current-inventory">
+        <Translate contentKey="global.menu.entities.currentInventory" />
+      </MenuItem>
       {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
     </>
   );
--- frontend/src/main/webapp/app/entities/reducers.ts
+++ frontend/src/main/webapp/app/entities/reducers.ts
@@ -1,6 +1,8 @@
+import currentInventory from 'app/entities/current-inventory/current-inventory.reducer';
 /* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 
 const entitiesReducers = {
+  currentInventory,
   /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
 };
 
