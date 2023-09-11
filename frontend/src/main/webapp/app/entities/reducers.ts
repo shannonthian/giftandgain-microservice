36e30cad3a9cda51a0e12d3c77cf9e25ed66977b
bobbest <banditben.1994@gmail.com>
2023-09-11 21:18:07 +0800

    added UserService for login and authentication

--- frontend/src/main/webapp/app/entities/reducers.ts
+++
@@ -1,9 +0,0 @@
-import currentInventory from 'app/entities/current-inventory/current-inventory.reducer';
-/* jhipster-needle-add-reducer-import - JHipster will add reducer here */
-
-const entitiesReducers = {
-  currentInventory,
-  /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
-};
-
-export default entitiesReducers;
