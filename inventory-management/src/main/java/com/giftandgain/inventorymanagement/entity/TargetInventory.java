6aa842f53332917c27942bc7a4a74ccfdca4875f
comatrix <comatrix70@gmail.com>
2023-09-22 15:54:49 +0800

    Revert "Revert "Merge branch 'main' of https://github.com/shannonthian/giftandgain-microservice into frontend""
    This reverts commit 8e016b125798bed591a26b08a0b318fe45a19515.

---
+++ inventory-management/src/main/java/com/giftandgain/inventorymanagement/entity/TargetInventory.java
@@ -0,0 +1,101 @@
+package com.giftandgain.inventorymanagement.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "tb_target_inventory")
+public class TargetInventory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "target_id")
+	private Long targetId;
+
+	@Column(name = "category")
+	private String category;
+
+	@Column(name = "unit")
+	private String unit;
+
+	@Column(name = "target_quantity")
+	private int targetQuantity;
+
+	@Column(name = "target_month")
+	private String targetMonth;
+
+	@Column(name = "target_year")
+	private String targetYear;
+
+	public TargetInventory() {
+
+	}
+
+	@Override
+	public String toString() {
+		return "TargetInventory [targetId=" + targetId + ", category=" + category + ", unit=" + unit + ", targetQuantity="
+				+ targetQuantity + ", targetMonth=" + targetMonth + ", targetYear=" + targetYear + "]";
+	}
+
+	public TargetInventory(Long targetId, String category, String unit, int targetQuantity, String targetMonth,
+			String targetYear) {
+		super();
+		this.targetId = targetId;
+		this.category = category;
+		this.unit = unit;
+		this.targetQuantity = targetQuantity;
+		this.targetMonth = targetMonth;
+		this.targetYear = targetYear;
+	}
+
+	public Long getTargetId() {
+		return targetId;
+	}
+
+	public void setTargetId(Long targetId) {
+		this.targetId = targetId;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public int getTargetQuantity() {
+		return targetQuantity;
+	}
+
+	public void setTargetQuantity(int targetQuantity) {
+		this.targetQuantity = targetQuantity;
+	}
+
+	public String getTargetMonth() {
+		return targetMonth;
+	}
+
+	public void setTargetMonth(String targetMonth) {
+		this.targetMonth = targetMonth;
+	}
+
+	public String getTargetYear() {
+		return targetYear;
+	}
+
+	public void setTargetYear(String targetYear) {
+		this.targetYear = targetYear;
+	}
+
+}
--- inventory-management/src/main/resources/data.sql
+++ inventory-management/src/main/resources/data.sql
@@ -1,12 +1,29 @@
 insert into tb_inventory
-(id, item_name, quantity, type_of_donation, expiry_date, created_date)
-values(10001, 'Rice', 200, 'Non-perishable', '2025-09-10','2023-09-10T22:12:16.524557');
+(inventory_id, item_name, category, unit, received_quantity, expiry_date, created_date, remarks)
+values(10001, 'Rice', 'Grains', 'kg', 200, '2025-09-10','2023-09-10T22:12:16.524557', 'this is a dry food');
 insert into tb_inventory
-(id, item_name, quantity, type_of_donation, expiry_date, created_date)
-values(10002, 'Dairy products', 10, 'Perishable', '2025-10-10','2023-09-08T22:12:16.524557');
+(inventory_id, item_name, category, unit, received_quantity, expiry_date, created_date, remarks)
+values(10002, 'Noodles', 'Grains', 'kg', 30, '2025-09-10','2023-09-10T22:12:16.524557', 'this is a dry food');
 insert into tb_inventory
-(id, item_name, quantity, type_of_donation, expiry_date, created_date)
-values(10003, 'Frozen vegetables', 160, 'Frozen foods', '2028-09-10','2023-10-08T22:12:16.524557');
+(inventory_id, item_name, category, unit, received_quantity, expiry_date, created_date, remarks)
+values(10003, 'Tuna Can', 'Canned Food', 'counts', 10, '2025-09-10','2022-09-10T22:12:16.524557', 'this is a dry food');
 insert into tb_inventory
-(id, item_name, quantity, type_of_donation, expiry_date, created_date)
-values(10004, 'Frozen vegetables', 160, 'Frozen foods', '2028-09-10','2023-01-08T22:12:16.524557');
+(inventory_id, item_name, category, unit, received_quantity, expiry_date, created_date, remarks)
+values(10004, 'Milk', 'Beverages', 'ml', 300, '2025-10-10','2023-09-10T22:12:16.524557', 'this is a dry food');
+insert into tb_inventory
+(inventory_id, item_name, category, unit, received_quantity, expiry_date, created_date, remarks)
+values(10005, 'Frozen Strawberries', 'Frozen Food', 'packs', 10, '2025-09-10','2023-09-10T22:12:16.524557', 'this is a dry food');
+insert into tb_inventory
+(inventory_id, item_name, category, unit, received_quantity, expiry_date, created_date, remarks)
+values(10006, 'Milo Drinks', 'Beverages', 'ml', 300, '2025-09-10','2023-10-10T22:12:16.524557', 'this is a dry food');
+insert into tb_target_inventory
+(target_id, category, unit, target_quantity, target_month, target_year)
+values(101, 'Grains', 'kg', 100, '09', '2023');
+insert into tb_target_inventory
+(target_id, category, unit, target_quantity, target_month, target_year)
+values(102, 'Frozen Food', 'packs', 250, '09', '2023');
+insert into tb_target_inventory
+(target_id, category, unit, target_quantity, target_month, target_year)
+values(103, 'Beverages', 'ml', 250, '09', '2023');
+
+
