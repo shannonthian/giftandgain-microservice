2d278d0d36cc6eb4bd086f0f50e2bc4d7f2a3fcb
comatrix <comatrix70@gmail.com>
2023-09-24 11:35:30 +0800

    Revert "Merge remote-tracking branch 'origin/userservice'"
    This reverts commit 206c472f83abf0a76c523e0866a6a9691cf1c648, reversing
    changes made to 7bf119b5c8b114e65e3b4930cc0a613a0e57cc5f.

--- src/main/java/com/giftandgain/userservice/repository/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.giftandgain.userservice.repository;
-
-import com.giftandgain.userservice.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
