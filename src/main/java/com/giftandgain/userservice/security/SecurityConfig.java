8c8655d708830d1a6d8699ca2673a93aa8b3e3f4
bobbest <banditben.1994@gmail.com>
2023-10-02 01:22:14 +0800

    baseline for login

---
+++ src/main/java/com/giftandgain/userservice/security/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.giftandgain.userservice.security;
+
+import com.giftandgain.userservice.filter.CustomAuthenticationFilter;
+import com.giftandgain.userservice.filter.CustomAuthorizationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    // by default, will have default "user" and a generated password
+    // this was using session
+    // we don't want to use this, hence override and disable the default method
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        // now urls have to go to ./api/login - to overwrite the default ./login in CustomAuthFilter
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        // disable default login
+        http.csrf().disable();
+        // stateless session
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        // Order of API path matters
+        // this login path is inherited from CustomAuthentication Filter (we didn't create this in controller)
+        // http.authorizeRequests().antMatchers("/login").permitAll();
+        http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
+        http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+        // permit all users to use the app
+        http.authorizeRequests().anyRequest().authenticated();
+        // authentication filter
+        http.addFilter(customAuthenticationFilter);
+        // filter before because need to intercept every request first with this filter
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
