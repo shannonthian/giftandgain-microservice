ee85b6a60486b7eb8622d8a0ef6f0dc1d53ccf4c
bobbest <banditben.1994@gmail.com>
2023-10-02 01:12:00 +0800

    Baseline login with JWT

--- src/main/java/com/giftandgain/userservice/UserserviceApplication.java
+++ src/main/java/com/giftandgain/userservice/UserserviceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -19,6 +21,14 @@ public class UserserviceApplication {
 
 	// This will run when application is started
 	// e.g. to seed data
+
+	// Tell spring how to load the bean for password encoder
+	// so when spring runs, this bean is available to be picked up by the application
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Bean
 	CommandLineRunner run(UserService userService) {
 		return args -> {
--- src/main/java/com/giftandgain/userservice/service/UserServiceImpl.java
+++ src/main/java/com/giftandgain/userservice/service/UserServiceImpl.java
@@ -4,22 +4,49 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.giftandgain.userservice.models.Role;
 import com.giftandgain.userservice.models.User;
+import net.bytebuddy.build.Plugin;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.giftandgain.userservice.repository.RoleRepo;
 import com.giftandgain.userservice.repository.UserRepo;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     // Lombok helps to create the constructors with @RequiredArgsConstructor
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user == null) {
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found in the database: {}", username);
+        }
+        // need to return GrantedAuthority type for spring User model
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> { authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities );
+    }
 
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the database", user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 
@@ -49,4 +76,5 @@ public class UserServiceImpl implements UserService {
         log.info("Fetching all users");
         return userRepo.findAll();
     }
+
 }
