a3bd91621be96239698d6ac783d9afe1ef1beffe
Sim Yin Ci <simyinci@gmail.com>
2023-10-17 02:21:45 +0800

    Added Download Performance Report Page

--- frontend/src/main/webapp/app/entities/target-inventory/index.tsx
+++ frontend/src/main/webapp/app/entities/target-inventory/index.tsx
@@ -7,11 +7,13 @@ import TargetInventory from './target-inventory';
 import TargetInventoryDetail from './target-inventory-detail';
 import TargetInventoryUpdate from './target-inventory-update';
 import TargetInventoryDeleteDialog from './target-inventory-delete-dialog';
+import TargetInventoryReportDownload from './target-inventory-report-download';
 
 const TargetInventoryRoutes = () => (
   <ErrorBoundaryRoutes>
     <Route index element={<TargetInventory />} />
     <Route path="new" element={<TargetInventoryUpdate />} />
+    <Route path="report-download" element={<TargetInventoryReportDownload />} />
     <Route path=":id">
       <Route index element={<TargetInventoryDetail />} />
       <Route path="edit" element={<TargetInventoryUpdate />} />
