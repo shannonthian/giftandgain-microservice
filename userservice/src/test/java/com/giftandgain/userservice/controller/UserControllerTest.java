b79725fa7c869e3a470739ea4e546b5d823cce72
bobbest <banditben.1994@gmail.com>
2023-10-23 16:49:21 +0800

    [Fix,Feat] - Fixed register and updateUser API, added JunitTests

---
+++ userservice/src/test/java/com/giftandgain/userservice/controller/UserControllerTest.java
@@ -0,0 +1,34 @@
+package com.giftandgain.userservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+    @Test
+    public void loginTest() {
+        int num = 2 + 2;
+        assertEquals(4, num);
+        assertNotNull(num);
+    }
+
+    @Test
+    public void getUserTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void updateUserTest() {}
+
+    @Test
+    public void getUsersTest() {}
+
+    @Test
+    public void saveUserTest() {}
+
+    @Test
+    public void registerTest() {}
+
+    @Test
+    public void addRoleToUserTest() {}
+}
