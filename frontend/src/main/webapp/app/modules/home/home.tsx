0a51b99a78470465cfcba37a549670b3bdb0ffc7
bobbest <banditben.1994@gmail.com>
2023-10-24 23:35:23 +0800

    Testing CICD

--- frontend/src/main/webapp/app/modules/home/home.tsx
+++ frontend/src/main/webapp/app/modules/home/home.tsx
@@ -24,7 +24,7 @@ export const Home = () => {
           <b>
             Gift & Gain Application aims to be a leading user-friendly digital platform for food charity organizations and potential donors.
             Our platform will streamline the process of managing inventory of donation items received by food charity organizations, and
-            keep potential donors informed about items that are in high demand.
+            keep potential donors informed about items that are in high demand. Browse our webpage to find out more!
           </b>
         </p>
         {/*
